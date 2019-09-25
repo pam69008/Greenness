1793fb5a29160a5b8ca7786b21734feb2abe6eda
Pierre-Alain Monney <pierrealain.monney@gmail.com>
2019-09-25 16:46:02 +0200

    Mise a jour menu en php

--- navbar.php
+++ navbar.php
@@ -1,27 +1,31 @@
-      <!-- Navbar -->
+<!-- Navbar -->
+<?php
+$menuNavigation = [
+    'Accueil' => 'index.php',
+    'Produits' => 'product.php',
+    'Contact' => 'contact.php'
+]
+?>
 <nav class="navbar navbar-expand-sm navbar-light bg-light sticky-top">
-  <a href="index.php"><img class="glogo" src="images/glogo.png" "alt="logo greenness"></a>
-  <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+    <a href="index.php"><img class="glogo" src="images/glogo.png" "alt="logo greenness"></a>
+    <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
 
-  <div class="collapse navbar-collapse text-center" id="navbarNav">
-    <ul class="navbar-nav ml-auto mr-auto">
-      <li>
-        <a class="nav-link" href="index.php">Accueil</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="product.php">Produits</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="contact.php">Contact</a>
-      </li>
-    </ul>
-  </div>
+    <div class="collapse navbar-collapse text-center" id="navbarNav">
+        <ul class="navbar-nav ml-auto mr-auto">
+            <?php foreach ($menuNavigation as $menu => $link) {
+                echo "<li><a class ='nav-link' href =$link</a>$menu </li>";
+            }
+            ?>
+        </ul>
+    </div>
     <div id="panier">
-      <div id="basket-container" class="position-relative">
-        <span class="position-absolute fa fa-shopping-basket fa-lg" ></span>
-        <span id="nbItems"></div>
-      </div>
+        <div id="basket-container" class="position-relative">
+            <span class="position-absolute fa fa-shopping-basket fa-lg"></span>
+            <span id="nbItems"></div>
+    </div>
     </div>
 </nav>
+
