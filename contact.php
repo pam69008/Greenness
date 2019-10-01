4dcb04b2229daafc4bee6831693e7d5f1789d2d9
Pierre-Alain Monney <pierrealain.monney@gmail.com>
2019-10-01 11:01:33 +0200

    mise a jour form

--- contact.php
+++ contact.php
@@ -7,12 +7,13 @@ include 'header.php'; ?>
     <div class="container d-flex flex-column flex-md-column-reverse">
         <section id="form" class="col -6">
             <h2 id="Contact">Contact</h2>
-            <form action="succes.php" method="POST">
+            <?php echo errors($arrayForm);?>
+            <form action="contact.php" method="POST">
                 <div class="row">
                     <div class= col-12 >
                     <?php echo contactTop($arrayForm);?>
-                    </div>
 
+                    </div>
 
                     <div class="col-12">
                         <label for="message">Message : </label>
--- function.php
+++ function.php
@@ -8,17 +8,18 @@ $menuNavigation = [
 
 /*titre */
 $pages = [
-    "/index.php"=> "Accueil",
+    "/index.php" => "Accueil",
     "/product.php" => "Produits",
     "/contact.php" => "Contact",
     "/succes.php" => "Succes"
 ];
 
 /*fonction titre */
-function displayTitre($page) {
+function displayTitre($page)
+{
     $currentPages = $_SERVER["REQUEST_URI"];
     $title = $page[$currentPages];
-    echo   "<title> $title </title>";
+    echo "<title> $title </title>";
 
 }
 
@@ -31,28 +32,79 @@ function navbarOne($tableau)
 
     }
 }
-/*tableau Formulaire */
 
+
+
+
+/* fonction pour ajout d'un champ dans le formulaire*/
+function test_input($data)
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+$nameErrors="";
+$name=$firstname=$email=$phone="";
+function errors($tableauTwo)
+{
+        if ($_POST) {
+            $errors = 0;
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+                if (empty($_POST[$tableauTwo[0]])) {
+                    $nameErrors ="Le nom est invalide ";
+                    $errors++;
+
+                }
+
+                else {
+                    $name = test_input($_POST[$tableauTwo[0]]);
+                }
+                if (empty($_POST[$tableauTwo[1]])) {
+                    $nameErrors = "Le prénom est invalide";
+                    $errors++;
+
+
+                } else {
+                    $firstname = test_input($_POST[$tableauTwo[1]]);
+                }
+                if (empty($_POST[$tableauTwo[2]])) {
+                    $nameErrors = "Le mail est invalide";
+                    $errors++;
+
+                } else {
+                    $email = test_input($_POST[$tableauTwo[2]]);
+                }
+                if (empty($_POST[$tableauTwo[3]])) {
+                    $nameErrors = "Le téléphone est invalide";
+                    $errors++;
+
+                } else {
+                    $phone = test_input($_POST[$tableauTwo[3]]);
+                }
+
+            }
+            if (!$nameErrors) {
+                header('location: /succes.php');
+            }else{
+                echo $nameErrors;
+            }
+        }
+
+}
+
+/*tableau Formulaire */
 $arrayForm = [
     'Nom',
     "Prénom",
     'Email',
     'Télephone',
 ];
-/* fonction pour ajout d'un champ dans le formulaire*/
 
-function contactTop($tableauTwo)
-{
-    foreach ($tableauTwo as $users) {
-        echo " <label for= name> $users: </label>";
-        echo "<input type=text name=$users  class=form-control>";
-        if ($_SERVER["REQUEST_METHOD"] === "POST") {
-            if (empty($_POST['$users'])) {
-                echo "Veuillez rentrer vos informations";
-            } else {
-                header('Location: succes.php');
-            }
-        }
-
-    }
+function contactTop($tableauThree){
+    foreach($tableauThree as $users){
+    echo " <label for= name> $users: </label>";
+    echo "<input type=text name=$users  class=form-control>";
+}
 }
